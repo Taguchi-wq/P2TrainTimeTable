060d152bc99a824dd07f09111ffec14bfdae27de
cmStudent <cmstudent@cmStudentnoMacBook-Air.local>
2021-07-15 20:26:20 +0900

    Initial Commit

---
+++ P2TrainTimeTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P2TrainTimeTable
+//
+//  Created by cmStudent on 2021/07/15.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
