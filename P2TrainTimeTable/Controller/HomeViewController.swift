cbaf96445bde4fc752328de6932d1c20281d1b2e
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-15 20:44:04 +0900

    ホーム画面の作成

--- P2TrainTimeTable/Controller/HomeViewController.swift
+++ P2TrainTimeTable/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  P2TrainTimeTable
 //
 //  Created by cmStudent on 2021/07/15.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
-
