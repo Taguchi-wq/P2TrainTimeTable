900d21768d0386a9f5f39eefc348fafefe7a85c9
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-15 21:33:01 +0900

    検索結果画面を作成

---
+++ P2TrainTimeTable/Controller/SearchResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultViewController.swift
+//  P2TrainTimeTable
+//
+//  Created by cmStudent on 2021/07/15.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
