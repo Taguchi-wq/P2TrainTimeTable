4ad6e2b243ec84828201e5ede7c1a88b354be0e4
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-17 18:03:46 +0900

    タイムテーブル画面を作成

---
+++ P2TrainTimeTable/Controller/TimetableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableViewController.swift
+//  P2TrainTimeTable
+//
+//  Created by cmStudent on 2021/07/17.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class TimetableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
